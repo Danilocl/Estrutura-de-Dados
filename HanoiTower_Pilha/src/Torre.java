d637284a8ceba4cbc02991b42049756f89870213
ads.2n <ads.2n@L606-18.senacrjedu.local>
2017-06-13 22:14:37 -0300

    Comit

---
+++ HanoiTower_Pilha/src/Torre.java
@@ -0,0 +1,20 @@
+	
+public class Torre {
+	
+	private int n;
+	private int source;
+	private int destiny;
+	private int aux;
+	
+	public Torre(int n, int source, int destiny, int aux) {
+		super();
+		this.n = n;
+		this.source = source;
+		this.destiny = destiny;
+		this.aux = aux;
+	} 
+	
+	
+	
+}	
+	
