6740e8f57467c39ce02a1d7dd8c2a757804afac7
Danilo <danilocalessa@gmail.com>
2017-06-19 15:59:18 -0300

    cópia de um código achado no site "guj" que implementa a torre de hanoi iterativa

---
+++ HanoiTower_Pilha/src/Test.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+
+	int disks;
+	private List<Pilha> discos = new ArrayList<Pilha>();
+
+	Scanner in = new Scanner(System.in);
+
+	// Apenas pega os dados necessários
+	public void dados() {
+
+		Pilha p = new Pilha();
+
+		System.out.println("Digite o número de discos");
+		disks = in.nextInt();
+
+	}
+
+	//realiza os movimentos
+	public void mover() {
+		int b = 1;
+		while (disks >= b) {
+			int jump = (int) Math.pow(2, b);
+		}
+	}
+
+	private void printf(String string, int o2, int d2) {
+		// TODO Auto-generated method stub
+
+	}
+}
